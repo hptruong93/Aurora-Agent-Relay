778fd6a0908d33e378e777c58582dc5b17f7ed37
iamtheone188 <iamtheone188@hotmail.com>
2014-08-01 14:30:36 -0400

    Changed config file to match APU parameters

--- libtins/revised_version/config.h
+++ libtins/revised_version/config.h
@@ -9,8 +9,8 @@
 namespace Config {
     
     Tins::HWAddress<6> BROADCAST("ff:ff:ff:ff:ff:ff");
-    Tins::HWAddress<6> HOSTAPD("40:d8:55:04:22:84");//"00:21:5d:22:97:8c");
-    Tins::HWAddress<6> PC_ENGINE("84:8f:69:cb:69:cd");
+    Tins::HWAddress<6> HOSTAPD("02:00:00:00:00:00");//"00:21:5d:22:97:8c");
+    Tins::HWAddress<6> PC_ENGINE("00:0D:B9:34:17:29");
     Tins::HWAddress<6> WARP("40:d8:55:04:22:85");
 
     static uint8_t default_radio_tap_buffer[14] = {0x00, 0x00, 0x0e, 0x00, 0x0e, 0x00, 0x00, 0x00, 0x00, 0x02, 0x71, 0x09, 0xa0, 0x00};
