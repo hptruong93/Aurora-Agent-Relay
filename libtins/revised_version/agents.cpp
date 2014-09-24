63918df83111043dca982edb9ddcc2116fdf005f
everfor <alan.yang1994@gmail.com>
2014-09-24 12:55:17 -0400

    Minor fixes and supports for multithreading

--- libtins/revised_version/agents.cpp
+++ libtins/revised_version/agents.cpp
@@ -1,29 +1,21 @@
 #include "mon_to_warp_agent.h"
 #include "wlan_to_warp_agent.h"
 #include "warp_to_wlan_agent.h"
+#include <thread>
+
 
 #include <iostream>
 
-#include <stdio.h>
-#include <string>
-#include <exception>
-
-#include "config.h"
-#include "util.h"
-#include <tins/tins.h>
-#include "../send_receive_module/warp_protocol_sender.h"
-#include "../send_receive_module/fragment_receiver.h"
-#include "../warp_protocol/warp_protocol.h"
-
-using namespace Tins;
-using namespace Config;
-using namespace std;
-
 using namespace RelayAgents;
+using namespace std;
 
 int main(int argc, char *argv[])
 {
 	// Test
 	WlanToWarpAgent test_wtp_agent;
-	test_wtp_agent.run(argc, argv);
+	thread t1(&WlanToWarpAgent::run, &test_wtp_agent, argc, argv);
+
+	t1.join();
+
+	return 0;
 }
