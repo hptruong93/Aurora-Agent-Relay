c702e393ae4931e6f3182f37725df0d824595542
everfor <alan.yang1994@gmail.com>
2014-10-03 11:28:19 -0400

    Make ready for Alfreds json message

--- libtins/relay_agent_module/agents.cpp
+++ libtins/relay_agent_module/agents.cpp
@@ -2,45 +2,123 @@
 #include "wlan_to_warp_agent.h"
 #include "warp_to_wlan_agent.h"
 #include "agents.h"
+
 #include <thread>
+#include <iostream>
 
 #include <zmq.hpp>
-#include <iostream>
+#include <jansson.h>
 
 using namespace RelayAgents;
 using namespace std;
 
-Agents::Agents(string init_port) : port(init_port)
+AgentFactory::AgentFactory(string init_port) : port(init_port), util(AgentUtil())
 {
-	this->context = unique_ptr<zmq::context_t>(new zmq::context_t(1));
-	this->socket = unique_ptr<zmq::socket_t>(new zmq::socket_t(*(this->context.get()), ZMQ_REP));
+    this->context = unique_ptr<zmq::context_t>(new zmq::context_t(1));
+    this->socket = unique_ptr<zmq::socket_t>(new zmq::socket_t(*(this->context.get()), ZMQ_REP));
 
-	string address = "tcp://*:" + this->port;
-	(this->socket.get())->bind(address.c_str());
+    string address = "tcp://*:" + this->port;
+    (this->socket.get())->bind(address.c_str());
 }
 
-void Agents::spin()
+void AgentFactory::spin()
 {
-	zmq::message_t received_msg;
-	zmq::message_t send_message(DEFAULT_MSG_SIZE);
-	while(true)
-	{
-		this->socket.get()->recv(&(received_msg));
+    zmq::message_t received_msg;
+    zmq::message_t send_message(DEFAULT_MSG_SIZE);
+    while(true)
+    {
+        this->socket.get()->recv(&(received_msg));
 
-		// Do parsing
+        // Do parsing
 
-		send_message.rebuild();
-		this->socket.get()->send(send_message);
-	}
+        send_message.rebuild();
+        this->socket.get()->send(send_message);
+    }
+}
+
+AgentUtil::AgentUtil() : in_interface(nullptr), out_interface(nullptr), mac_addr(nullptr), parameters(0)
+{
+
+}
+
+int AgentUtil::parse_json(const char *json_string)
+{
+    this->reset();
+    
+    json_t *root;
+    json_error_t error;
+
+    root = json_loads(json_string, 0, &error);
+    if (!root)
+    {
+        cout<<"ERROR: on line "<<error.line<<": "<<error.text<<endl;
+
+        return 1;
+    }
+
+    // TOdO: Json Parsing
+
+    json_decref(root);
+
+    return 0;
+}
+
+int AgentUtil::get_parameter_count() const
+{
+    return this->parameters;
+}
+
+char* AgentUtil::transfer_parameter(const char *parameter_name)
+{
+    if (strcmp(parameter_name, INTERFACE_IN) == 0)
+    {
+        return this->in_interface.release();
+    }
+    else if (strcmp(parameter_name, INTERFACE_OUT) == 0)
+    {
+        return this->out_interface.release();
+    }
+    else if (strcmp(parameter_name, MAC_ADDRESS) == 0)
+    {
+        return this->mac_addr.release();
+    }
+
+    return NULL;
+}
+
+char* AgentUtil::get_parameter(const char *parameter_name) const
+{
+    if (strcmp(parameter_name, INTERFACE_IN) == 0)
+    {
+        return this->in_interface.get();
+    }
+    else if (strcmp(parameter_name, INTERFACE_OUT) == 0)
+    {
+        return this->out_interface.get();
+    }
+    else if (strcmp(parameter_name, MAC_ADDRESS) == 0)
+    {
+        return this->mac_addr.get();
+    }
+
+    return NULL;    
+}
+
+void AgentUtil::reset()
+{
+    this->in_interface.reset();
+    this->out_interface.reset();
+    this->mac_addr.reset();
+    this->parameters = 0;
 }
 
 int main(int argc, char *argv[])
 {
-	// Test
-	WlanToWarpAgent test_wtp_agent;
-	thread t1(&WlanToWarpAgent::run, &test_wtp_agent, argc, argv);
+    // Test
+    WlanToWarpAgent test_wtp_agent;
+    thread t1(&WlanToWarpAgent::run, &test_wtp_agent, argc, argv);
 
-	t1.join();
+    t1.join();
 
-	return 0;
+    return 0;
 }
--- libtins/relay_agent_module/agents.h
+++ libtins/relay_agent_module/agents.h
@@ -1,22 +1,45 @@
 #include <string>
 #include <zmq.hpp>
+#include <jansson.h>
 
 #ifndef AGENTS_H_
 #define AGENTS_H_
 
 #define DEFAULT_MSG_SIZE        1024
+#define INTERFACE_IN            "in_interface"
+#define INTERFACE_OUT           "out_interface"
+#define MAC_ADDRESS             "mac_addr"
 
 using namespace std;
 
-class Agents {
-    string port;
-    unique_ptr<zmq::context_t> context;
-    unique_ptr<zmq::socket_t> socket;
-    public:
-        Agents(string init_port = "5556");
-        void spin();
-        // void spawn_agent_thread();
-};
+namespace RelayAgents {
+    // Utility class
+    class AgentUtil {
+        unique_ptr<char> in_interface;
+        unique_ptr<char> out_interface;
+        unique_ptr<char> mac_addr;
+        int parameters;
+        public:
+            AgentUtil();
+            int parse_json(const char *json_string);
+            int get_parameter_count() const;
+            char* transfer_parameter(const char *parameter_name);
+            char* get_parameter(const char *parameter_name) const;
+            void reset();
+    };
+    
+    // Receives command and initialize different agents
+    class AgentFactory {
+        string port;
+        AgentUtil util;
+        unique_ptr<zmq::context_t> context;
+        unique_ptr<zmq::socket_t> socket;
+        public:
+            AgentFactory(string init_port = "5556");
+            void spin();
+            // void spawn_agent_thread();
+    };
+}
 
 
 #endif
